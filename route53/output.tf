b9534a529745f22c77b703ae5fe48bdf01ce9dba
jsengun <j.sengun@gmail.com>
2021-11-03 18:58:47 +0000

    changes

--- route53/output.tf
+++ route53/output.tf
@@ -1,3 +1,15 @@
-data "aws_outposts_outpost_instance_types" "example" {
-  arn = data.aws_outposts_outpost.example.arn
+resource "aws_route53_zone" "main" {
+ name = "jsengun.com"
+}
+
+# resource "aws_route53_record" "dev" {
+#   name = "blog.jsengun.com"
+
+# }
+
+resource "aws_route53_record" "dev-ns" {
+  zone_id = "Z02071922GQJ74VZ692DR"
+  name    = "blog.jsengun.com"
+  type    = "NS"
+  records = ["127.0.0.1"]
 }
--- route53/route53.tf
+++ route53/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
-  zone_id = "Console >> Route53 >> Hosted Zone ID"
-  name    = "www.${jsengun.com}"
+  zone_id = "Z02071922GQJ74VZ692DR"
+  name    = "jsengun.com"
   type    = "A"
   ttl     = "300"
   records = ["127.0.0.1"]
